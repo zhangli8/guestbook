5cecc1598bd093a235057d1d00aba608cf2189aa
zhangli <zhangli@halfrin.com>
2018-07-25 22:10:50 +0800

    增加School实体类

---
+++ src/main/java/com/zhangli/School.java
@@ -0,0 +1,31 @@
+package com.zhangli;
+
+public class School {
+    private int id;
+    private String schoolname;
+    private int status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSchoolname() {
+        return schoolname;
+    }
+
+    public void setSchoolname(String schoolname) {
+        this.schoolname = schoolname;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
