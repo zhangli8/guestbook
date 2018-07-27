272e40179fcc05a8c45fb61b4b6aeb04a32f08f3
zhangli <zhangli@halfrin.com>
2018-07-27 08:32:53 +0800

    增加Guestbook实体类

---
+++ src/main/java/com/zhangli/Guestbook.java
@@ -0,0 +1,77 @@
+package com.zhangli;
+
+
+import java.sql.Timestamp;
+
+public class Guestbook {
+
+  private long id;
+  private String name;
+  private String title;
+  private String subTitle;
+  private java.sql.Timestamp date;
+  private String content;
+
+  public Guestbook(long id, String name, String title, String subTitle, Timestamp date, String content) {
+    this.id = id;
+    this.name = name;
+    this.title = title;
+    this.subTitle = subTitle;
+    this.date = date;
+    this.content = content;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+
+  public String getSubTitle() {
+    return subTitle;
+  }
+
+  public void setSubTitle(String subTitle) {
+    this.subTitle = subTitle;
+  }
+
+
+  public java.sql.Timestamp getDate() {
+    return date;
+  }
+
+  public void setDate(java.sql.Timestamp date) {
+    this.date = date;
+  }
+
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+}
